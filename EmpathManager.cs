98379375d2d0763d307febe59255dd877b41d082
Vaibhav Suri <40815703+vaibhav-suri@users.noreply.github.com>
2020-02-16 04:59:27 +0530

    Add files via upload
    Most Features Implemented

---
+++ EmpathManager.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EmpathManager : MonoBehaviour
+{
+    [SerializeField]
+    private string m_subscriptionKey = string.Empty;
+
+    [SerializeField]
+    int i = 0;
+    public Text rightpanel;
+    public Text leftpanel;
+
+    private Text empathResult;
+    public Text screentext;
+    private int count = 0;
+    private AudioClip micClip;
+    private float[] microphoneBuffer;
+    private bool flag = true;
+    private int head;
+    private int position;
+    private bool isRecording;
+    public GameObject scriptcont;
+    public int maxRecordingTime;
+    private const int samplingFrequency = 11025;
+    private string micDeviceName;
+    Dictionary<string, string> nlpresult;
+    const int HEADER_SIZE = 44;
+    const float rescaleFactor = 32767; //to convert float to Int16
+
+    public void Start()
+    {
+        nlpresult = scriptcont.GetComponent<nlpapitime>().emotionalsentences;
+       // print(nlpresult.Keys.ElementAt(1));
+
+        micDeviceName = Microphone.devices[2];
+        foreach (var device in Microphone.devices)
+        {
+            Debug.Log("Name: " + device);
+        }
+    }
+
+    public void ButtonClicked()
+    {
+        rightpanel.text = "";
+        leftpanel.text = "";
+        if (!isRecording)
+        {
+            StartCoroutine(RecordingForEmpathAPI());
+        }
+
+
+    }
+
+    public IEnumerator RecordingForEmpathAPI()
+    {
+        print(nlpresult.Keys.ElementAt(i));
+        screentext.text = nlpresult.Keys.ElementAt(i);
+        RecordingStart();
+        yield return new WaitForSeconds(maxRecordingTime);
+
+        RecordingStop();
+
+        yield return null;
+
+    }
+
+    public void RecordingStart()
+    {
+        StartCoroutine(WavRecording(micDeviceName, maxRecordingTime, samplingFrequency));
+    }
+
+    public void RecordingStop()
+    {
+        isRecording = false;
+        position = Microphone.GetPosition(null);
+        Microphone.End(micDeviceName);
+        Debug.Log("Recording end");
+        byte[] empathByte = WavUtility.FromAudioClip(micClip);
+        StartCoroutine(Upload(empathByte));
+    }
+
+    IEnumerator Upload(byte[] wavbyte)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("apikey", m_subscriptionKey);
+        form.AddBinaryData("wav", wavbyte);
+        string receivedJson = null;
+
+        using (UnityWebRequest www = UnityWebRequest.Post("https://api.webempath.net/v2/analyzeWav", form))         {             yield return www.SendWebRequest();              if (www.isNetworkError || www.isHttpError)             {                 Debug.Log(www.error);             }             else             {                 receivedJson = www.downloadHandler.text;
+                //  Debug.Log(receivedJson);
+            }         }          EmpathData empathData = ConvertEmpathToJson(receivedJson);         empathResult.text = ConvertEmpathDataToString(empathData);
+
+    }
+
+    public IEnumerator WavRecording(string micDeviceName, int maxRecordingTime, int samplingFrequency)
+    {
+        Debug.Log("Recording start");
+        //Recording開始
+        isRecording = true;
+
+        //Buffer
+        microphoneBuffer = new float[maxRecordingTime * samplingFrequency];
+        //録音開始
+        micClip = Microphone.Start(deviceName: micDeviceName, loop: false,
+                                   lengthSec: maxRecordingTime, frequency: samplingFrequency);
+        yield return null;
+    }
+
+    public EmpathData ConvertEmpathToJson(string json)
+    {
+        Debug.AssertFormat(!string.IsNullOrEmpty(json), "Jsonの取得に失敗しています。");
+
+        EmpathData empathData = null;
+
+        try
+        {
+            empathData = JsonUtility.FromJson<EmpathData>(json);
+        }
+        catch (System.Exception i_exception)
+        {
+            Debug.LogWarningFormat("Jsonをクラスへ変換することに失敗しました。exception={0}", i_exception);
+            empathData = null;
+        }
+        return empathData;
+    }
+
+    public string ConvertEmpathDataToString(EmpathData empathData)
+    {
+        string result;
+        if (empathData.error == 0)
+        {
+            int calm = empathData.calm;
+            int anger = empathData.anger;
+            int joy = empathData.joy;
+            int sorrow = empathData.sorrow;
+            int energy = empathData.energy;
+            Dictionary<string, int> finalemo = new Dictionary<string, int>() { { "Happy", joy }, { "Sad", sorrow }, { "Excited", energy }, { "Angry", anger }, { "Fear", 1 - calm }, { "Bored", 1 - energy } };
+            string nlpemotion = nlpresult.Values.ElementAt(i);
+            string bestemo = finalemo.FirstOrDefault(x => x.Value == finalemo.Values.Max()).Key;
+            rightpanel.text = bestemo;
+            leftpanel.text = nlpemotion;
+            result = "Emotion of line:" + i + " ,actual emotion : " + bestemo + ",expected emotion " + nlpemotion;
+            i = i + 1;
+
+        }
+        else
+        {
+            int error = empathData.error;
+            string msg = empathData.msg;
+            result = "error : " + error +
+                     "\nmsg : " + msg;
+        }
+        print(result);
+        return result;
+    }
+}
---
+++ nlpapitime.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using ParallelDots;
+using Newtonsoft.Json.Linq;
+
+
+public class nlpapitime : MonoBehaviour
+{
+     public Dictionary<string, string> emotionalsentences;
+
+    string SpeechText = "I stand here today humbled by the task before us, grateful for the trust you have bestowed, mindful of the sacrifices borne by our ancestors. I thank President Bush for his service to our nation, as well as the generosity and cooperation he has shown throughout this transition.Forty-four Americans have now taken the presidential oath.The words have been spoken during rising tides of prosperity and the still waters of peace.Yet, every so often the oath is taken amidst gathering clouds and raging storms.At these moments, America has carried on not simply because of the skill or vision of those in high office, but because We the People have remained faithful to the ideals of our forbearers, and true to our founding documents";
+    paralleldots pd = new paralleldots("TZXtKoVbRO6L7iEfUEHaUeuNgeeF7dHaQ7BN5NKqeKc");
+    JArray bruh = JArray.Parse("[\"drugs are fun\", \"don\'t do drugs, stay in school\", \"lol you a fag son\", \"I have a throat infection\"]");
+
+    Dictionary<string,string> organizespeech(string texty)
+    {
+        List<string> sentences = new List<string>();
+        List<string> emotionsfinal = new List<string>();
+        Dictionary<string,string> finalarri = new Dictionary<string, string>();
+
+        //string[] spearator = { "."};
+        string[] strlist = texty.Split('.');
+        foreach (string s in strlist)
+        {
+            sentences.Add(s);
+        }
+        foreach (string i in sentences)
+        {
+
+            string emo = pd.emotion(i);
+            
+            JObject jsonObj = JObject.Parse(emo);
+            Dictionary<string, object> emodict = jsonObj.ToObject<Dictionary<string, object>>();
+
+           Dictionary<string, float> finalemodict = JObject.FromObject(emodict["emotion"]).ToObject<Dictionary<string, float>>();
+            var bestemo = finalemodict.FirstOrDefault(x => x.Value == finalemodict.Values.Max()).Key;
+            emotionsfinal.Add(bestemo);
+            // print(finalemodict["Happy"]);
+
+        }
+        for(int i=0;i<emotionsfinal.Count();i++)
+        {
+            finalarri.Add(sentences[i], emotionsfinal[i]);
+       
+        }
+        //print(finalarri.Count);
+
+        return (finalarri);
+    }
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {//string emotion_batch = pd.emotion_batch(bruh);
+     //  print("emotion batch");
+     //print(emotion_batch);
+        emotionalsentences = organizespeech(SpeechText);
+        //print(emotionalsentences);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
